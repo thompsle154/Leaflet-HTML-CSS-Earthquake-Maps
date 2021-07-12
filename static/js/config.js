ddf99caaab4204c9db5e378e1a70e8c54a937550
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-07-12 16:46:34 -0600

    starter code

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,10 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiZm11a2FkZGFtIiwiYSI6ImNra2VuNDAwMDA4MnEydXBlamwxZGVvdzMifQ.8EkbQls971Zotst7-D8OyA";
+
+
+
+// Joe A (TA)  5:43 PM
+// ------------------------------------- 6/30 17 - Mapping Web - lecture 2 -------------------------------------
+// Add the Mapbox API key into config.js:
+// pk.eyJ1IjoibXVkaXRnb3lhbCIsImEiOiJjazgxMDlnNHowamhvM2xvNGRrZGpqaXo2In0.pBCjMq3dFkUslavxn9HZbQ
+// Remember to start a web server from the relevant directory with python -m http.server (edited) 
