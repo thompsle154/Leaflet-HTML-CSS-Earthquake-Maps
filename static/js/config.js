508d38841848a29056c8069bb971fcb5b8d4a76b
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-07-14 23:11:39 -0600

    updates

--- static/js/config.js
+++ static/js/config.js
@@ -1,7 +1,6 @@
 // API key
 const API_KEY = "pk.eyJ1IjoibXVkaXRnb3lhbCIsImEiOiJjazgxMDlnNHowamhvM2xvNGRrZGpqaXo2In0.pBCjMq3dFkUslavxn9HZbQ";
 
-//new api key I created on google maps api key   AIzaSyBZWjP-MS63IA5Y_yOhHXc0Yt_LH9QtZOE
 
 
 // Joe A (TA)  5:43 PM
