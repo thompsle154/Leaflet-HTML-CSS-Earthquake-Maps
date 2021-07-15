18b8874c4ea843c145bcc08ecd0e23be40d2256e
Leanne Thompson <78496051+thompsle154@users.noreply.github.com>
2021-07-14 22:53:13 -0600

    update

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,7 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiZm11a2FkZGFtIiwiYSI6ImNra2VuNDAwMDA4MnEydXBlamwxZGVvdzMifQ.8EkbQls971Zotst7-D8OyA";
+const API_KEY = "pk.eyJ1IjoibXVkaXRnb3lhbCIsImEiOiJjazgxMDlnNHowamhvM2xvNGRrZGpqaXo2In0.pBCjMq3dFkUslavxn9HZbQ";
 
+//new api key I created on google maps api key   AIzaSyBZWjP-MS63IA5Y_yOhHXc0Yt_LH9QtZOE
 
 
 // Joe A (TA)  5:43 PM
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,72 +1,91 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta 
-        charset="UTF-8">
-    <meta 
-        name="viewport" 
-        content="width=device-width, initial-scale=1.0">
-    <meta 
-        http-equiv="X-UA-Compatible" 
-        content="ie=edge">
+// Store our API endpoint inside queryUrl
+function buildUrl(){
+    // With this you can select a range of dates to display in the map
+    //------------------------------------------
+    // const
+    //     domain = "earthquake.usgs.gov",
+    //     endpoint = "/fdsnws/event/1/query",
+    //     format = "geojson",
+    //     starttime = "2020-05-01",
+    //     endtime = "2020-07-07",
+    //     maxLon = -169.5,
+    //     minLon = -163.8,
+    //     maxLat = 68.7,
+    //     minLat = 45.1;
 
-    <title>Leaflet Basic Map</title>
-
-    <!-- Leaflet CSS -->
-    <link 
-        rel="stylesheet" 
-        href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" 
-        integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-        crossorigin=""
-    />
-
-    <!-- d3 JavaScript -->
-    <script src="https://d3js.org/d3.v5.min.js"></script>
-
-    <!-- Our CSS -->
-    <link 
-        rel="stylesheet" 
-        type="text/css" 
-        href="static/css/style.css">
-
-</head>
-<body>
-    <!-- The div that holds our map -->
-    <div id="mapid"></div>
-
-    <!-- Leaflet JavaScript -->
-    <script 
-        src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-        integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-        crossorigin="">
-    </script>
+    // return `https://${domain}${endpoint}?format=${format}&starttime=${starttime}&endtime=${endtime}&maxlongitude=${maxLon}&minlongitude=${minLon}&maxlatitude=${maxLat}&minlatitude=${minLat}`;
     
-    <!-- API key -->
-    <script 
-        type="text/javascript" 
-        src="static/js/config.js">
-    </script>
+    // API URL of Significant Earthquakes for the past 7 Days
+    //------------------------------------------
+    return "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+} 
 
-    <!-- Our JavaScript -->
-    <script 
-        type="text/javascript" 
-        src="static/js/cities.js">
-    </script>
+// Function to get the api from https://earthquake.usgs.gov/
+(async function() {
+    const queryUrl = buildUrl();
+    const data = await d3.json(queryUrl);
+    // When we get a response, send the data.features object
+    createFeatures(data);
+})()
 
-    <script 
-        type="text/javascript" 
-        src="static/js/challenge_logic.js">
-    </script>
-
-</body>
-</html>
+function createFeatures(earthquakeData) {
 
 
-  //  API link
-  var USGSlink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
-  // need a GET request to the query URL
-  d3.json(USGSlink, function (data) {
-    console.log(data)
-    // Once we get a response, send the data.features object to the createFeatures function
-    createFeatures(data.features);
-  });
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>Magnitude: " + feature.properties.mag + "</p>" +
+        "<p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    const earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature
+    });
+
+    // Sending our earthquakes layer to the createMap function
+    createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+    // Adding title layer
+    const darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.dark",
+        accessToken: API_KEY
+    });
+
+    const lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.light",
+        accessToken: API_KEY
+    });
+    
+      // Define a baseMaps object to hold our base layers
+    const baseMaps = {
+            "Light Map": lightmap,
+            "Dark Map": darkmap
+    };
+
+    // Create overlay object to hold our overlay layer
+    const overlayMaps = {
+            Earthquakes: earthquakes
+    };
+
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    const myMap = L.map("map", {
+            center: [37.09, -95.71],
+            zoom: 5,
+            layers: [darkmap, earthquakes]
+    });
+
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+            collapsed: false
+    }).addTo(myMap);
+
+};
